b3d11386142bec3d78229f590e2362aacd1e475d
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-03-12 21:47:56 +0000

    Finished alert error and utils

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 
+import { AlertError } from './alert-error.js';
 import { Modal } from './modal.js';
+import { IMC, notNumber } from './utils.js';
 
 const form = document.querySelector('form');
 const inputWeight = document.querySelector('#weight');
 const inputHeight = document.querySelector('#height');
+
+inputHeight.oninput = () => AlertError.close();
+inputWeight.oninput = () => AlertError.close();
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -11,13 +16,23 @@ form.onsubmit = event => {
   const weight = inputWeight.value;
   const height = inputHeight.value;
 
+  const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height);
+
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+  
   const result = IMC(weight, height);
+  displayResultMessage(result);
+
+}
+
+function displayResultMessage(result){
   const message = `Seu IMC é de ${result}`;
 
   Modal.message.innerText = message;
   Modal.open()
-}
-
-function IMC(weight, height) {
-  return (weight / ((height / 100) **2)).toFixed(2)
 }
